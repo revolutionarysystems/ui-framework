bf7453b9012d05bf22c4079cbbf413cc524707d8
Adam Knight <adam@revolutionarysystems.co.uk>
2015-01-26 14:42:44 +0000

    Use new targz resource handler

--- src/main/java/uk/co/revsys/ui/framework/WebappResourceLoader.java
+++ src/main/java/uk/co/revsys/ui/framework/WebappResourceLoader.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import javax.servlet.ServletContext;
 import org.apache.commons.io.FileUtils;
+import org.springframework.web.context.ServletContextAware;
 import uk.co.revsys.resource.repository.model.Resource;
 import uk.co.revsys.resource.repository.provider.handler.ResourceHandler;
-import uk.co.revsys.resource.repository.provider.servlet.ServletContextAware;
 
 public class WebappResourceLoader implements ResourceHandler, ServletContextAware {
 
@@ -23,6 +23,7 @@ public class WebappResourceLoader implements ResourceHandler, ServletContextAwar
         String relativePath = resource.getPath().replace(path, "");
         File directory = new File(servletContext.getRealPath(relativePath));
         File file = new File(directory, resource.getName());
+        System.out.println("Writing file to " + file.getAbsolutePath());
         FileUtils.copyInputStreamToFile(contents, file);
     }
 
